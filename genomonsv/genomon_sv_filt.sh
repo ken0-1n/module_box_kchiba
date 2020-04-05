8657b0840dde2e9b0fbae4f42d27ee8cec58c684
Ubuntu <ubuntu@ip-172-31-35-92.ap-northeast-1.compute.internal>
2020-04-05 09:40:24 +0000

    add genomon sv filt

---
+++ genomonsv/genomon_sv_filt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+TUMOR_BAM=${TUMOR_BAM_DIR}/${TUMOR_BAM}
+
+ARGUMENT="${TUMOR_BAM} ${TUMOR_SV_DIR}/${TUMOR_SAMPLE} ${REFERENCE}"
+
+if [ ! _${CONTROL_PANEL} = "_None" ]
+then
+    ARGUMENT="${ARGUMENT} --non_matched_control_junction ${MERGED_JUNCTION}/${CONTROL_PANEL}.merged.junction.control.bedpe.gz"
+fi
+
+if [ ! _${NORMAL_SAMPLE} = "_None" ]
+then
+    NORMAL_BAM=${NORMAL_BAM_DIR}/${NORMAL_BAM}
+    ARGUMENT="${ARGUMENT} --matched_control_bam ${NORMAL_BAM}"
+
+    if [ ! _${CONTROL_PANEL} = "_None" ]
+    then
+        ARGUMENT="${ARGUMENT} --matched_control_label ${NORMAL_SAMPLE}"
+    fi
+fi
+
+ARGUMENT="${ARGUMENT} ${GENOMONSV_FILT_OPTION}"
+
+GenomonSV filt ${ARGUMENT}
+
+sv_utils filter ${TUMOR_SV_DIR}/${TUMOR_SAMPLE}.genomonSV.result.txt ${TUMOR_SV_DIR}/${TUMOR_SAMPLE}.genomonSV.result.filt.txt ${SV_UTILS_FILT_OPTION}
+
