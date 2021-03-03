1c022cf465d27c1593e23555434b886b29254cac
ken0-1n <ubuntu@ip-172-31-34-116.ap-northeast-1.compute.internal>
2021-03-03 05:54:04 +0000

    modify genomon_sv.sh

--- genomonsv/genomon_sv.sh
+++ genomonsv/genomon_sv.sh
@@ -3,21 +3,35 @@
 set -o errexit
 set -o nounset
 
-mkdir -p ${OUTPUT_DIR}
+mkdir -p ${TUMOR_OUTPUT_DIR}
 
 TUMOR_BAM=${TUMOR_BAM_DIR}/${TUMOR_BAM}
+NORMAL_BAM=${NORMAL_BAM_DIR}/${NORMAL_BAM}
 REFERENCE=${REFERENCE_DIR}/${REFERENCE_FILE}
 
-# GenomonSV parse
+# GenomonSV parse tumor
 GenomonSV \
     parse \
     ${TUMOR_BAM} \
-    ${OUTPUT_DIR}/${TUMOR_SAMPLE} \
+    ${TUMOR_OUTPUT_DIR}/${TUMOR_SAMPLE} \
     --reference ${REFERENCE} \
     ${GENOMONSV_PARSE_OPTION}
 
+
+if [ ! _${NORMAL_SAMPLE} = "_None" ]
+then
+    mkdir -p ${NORMAL_OUTPUT_DIR}
+    # GenomonSV parse normal
+    GenomonSV \
+        parse \
+        ${NORMAL_BAM} \
+        ${NORMAL_OUTPUT_DIR}/${NORMAL_SAMPLE} \
+        --reference ${REFERENCE} \
+        ${GENOMONSV_PARSE_OPTION}
+fi
+
 # GenomonSV filt
-argument="${TUMOR_BAM} ${OUTPUT_DIR}/${TUMOR_SAMPLE} ${REFERENCE}"
+argument="${TUMOR_BAM} ${TUMOR_OUTPUT_DIR}/${TUMOR_SAMPLE} ${REFERENCE}"
 
 if [ ! _${CONTROL_PANEL} = "_None" ]
 then
@@ -26,7 +40,6 @@ fi
 
 if [ ! _${NORMAL_SAMPLE} = "_None" ]
 then
-    NORMAL_BAM=${NORMAL_BAM_DIR}/${NORMAL_BAM}
     argument="${argument} --matched_control_bam ${NORMAL_BAM}"
 
     if [ ! _${CONTROL_PANEL} = "_None" ]
@@ -39,5 +52,4 @@ argument="${argument} ${GENOMONSV_FILT_OPTION}"
 
 GenomonSV filt ${argument}
 
-sv_utils filter ${OUTPUT_DIR}/${TUMOR_SAMPLE}.genomonSV.result.txt ${OUTPUT_DIR}/${TUMOR_SAMPLE}.genomonSV.result.filt.txt ${SV_UTILS_FILT_OPTION}
-
+sv_utils filter ${TUMOR_OUTPUT_DIR}/${TUMOR_SAMPLE}.genomonSV.result.txt ${TUMOR_OUTPUT_DIR}/${TUMOR_SAMPLE}.genomonSV.result.filt.txt ${SV_UTILS_FILT_OPTION}
